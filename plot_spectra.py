1c7eb080add09c60c7c5c453d322e0e202757693
Karl Gordon <kgordon@stsci.edu>
2020-05-12 11:25:01 -0400

    updates to get code to work again

--- plot_spectra.py
+++ plot_spectra.py
@@ -1,4 +1,4 @@
-from __future__ import (absolute_import, print_function, division)
+from __future__ import absolute_import, print_function, division
 
 import glob
 import argparse
@@ -6,19 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-from astropy.table import Table
+from astropy.table import Table, QTable
 from astropy import units as u
 
-from jwst_starnames import (astarnames_prime, astarnames_second,
-                            gstarnames_prime, gstarnames_second,
-                            wdstarnames_prime, wdstarnames_second)
+from jwst_starnames import (
+    astarnames_prime,
+    astarnames_second,
+    gstarnames_prime,
+    gstarnames_second,
+    wdstarnames_prime,
+    wdstarnames_second,
+)
 
-FNU = u.erg / (u.cm**2 * u.s * u.Hz)
-FLAM = u.erg / (u.cm**2 * u.s * u.AA)
+FNU = u.erg / (u.cm ** 2 * u.s * u.Hz)
+FLAM = u.erg / (u.cm ** 2 * u.s * u.AA)
 
 
-def set_params(lw=1.5, universal_color='#262626', fontsize=16):
-    '''Configure some matplotlib rcParams.
+def set_params(lw=1.5, universal_color="#262626", fontsize=16):
+    """Configure some matplotlib rcParams.
     Parameters
     ----------
     lw : scalar
@@ -27,53 +32,89 @@ def set_params(lw=1.5, universal_color='#262626', fontsize=16):
         Color of text and axis spines. Default is #262626, off-black
     fontsize : scalar
         Font size in points. Default is 12
-    '''
-    rc('font', size=fontsize)
-    rc('lines', linewidth=lw)
-    rc('patch', linewidth=lw, edgecolor='#FAFAFA')
-    rc('axes', linewidth=lw, edgecolor=universal_color,
-       labelcolor=universal_color,
-       axisbelow=True)
-    rc('image', origin='lower')
-    rc('xtick.major', width=lw*0.75)
-    rc('xtick.minor', width=lw*0.5)
-    rc('xtick', color=universal_color)
-    rc('ytick.major', width=lw*0.75)
-    rc('ytick.minor', width=lw*0.5)
-    rc('ytick', color=universal_color)
-    rc('grid', linewidth=lw)
-    rc('legend', loc='best', numpoints=1, scatterpoints=1, handlelength=1.5,
-        fontsize=fontsize, columnspacing=1, handletextpad=0.75)
+    """
+    rc("font", size=fontsize)
+    rc("lines", linewidth=lw)
+    rc("patch", linewidth=lw, edgecolor="#FAFAFA")
+    rc(
+        "axes",
+        linewidth=lw,
+        edgecolor=universal_color,
+        labelcolor=universal_color,
+        axisbelow=True,
+    )
+    rc("image", origin="lower")
+    rc("xtick.major", width=lw * 0.75)
+    rc("xtick.minor", width=lw * 0.5)
+    rc("xtick", color=universal_color)
+    rc("ytick.major", width=lw * 0.75)
+    rc("ytick.minor", width=lw * 0.5)
+    rc("ytick", color=universal_color)
+    rc("grid", linewidth=lw)
+    rc(
+        "legend",
+        loc="best",
+        numpoints=1,
+        scatterpoints=1,
+        handlelength=1.5,
+        fontsize=fontsize,
+        columnspacing=1,
+        handletextpad=0.75,
+    )
 
 
 def initialize_parser():
-    '''For running from command line, initialize argparse with common args
-    '''
-    ftypes = ['png', 'jpg', 'jpeg', 'pdf', 'ps', 'eps', 'rgba',
-              'svg', 'tiff', 'tif', 'pgf', 'svgz', 'raw']
+    """For running from command line, initialize argparse with common args
+    """
+    ftypes = [
+        "png",
+        "jpg",
+        "jpeg",
+        "pdf",
+        "ps",
+        "eps",
+        "rgba",
+        "svg",
+        "tiff",
+        "tif",
+        "pgf",
+        "svgz",
+        "raw",
+    ]
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--savefig', action='store',
-                        default=False, choices=ftypes,
-                        help='Save figure to a file')
-    parser.add_argument('--astars', help='Use A star models',
-                        action='store_true')
-    parser.add_argument('--gstars', help='Use G star models',
-                        action='store_true')
-    parser.add_argument('--wdstars', help='Use WD star models',
-                        action='store_true')
-    parser.add_argument('--primeonly', action='store_true',
-                        help='Only use prime designated sources')
-    parser.add_argument('--secondonly', action='store_true',
-                        help='Only use secondary designated sources')
-    parser.add_argument('--part1', action='store_true',
-                        help='Only check the part1 limited set of modes')
-    parser.add_argument("-t", "--target_obs", metavar=int, default=3,
-                        help="number of target observations per mode")
+    parser.add_argument(
+        "-s",
+        "--savefig",
+        action="store",
+        default=False,
+        choices=ftypes,
+        help="Save figure to a file",
+    )
+    parser.add_argument("--astars", help="Use A star models", action="store_true")
+    parser.add_argument("--gstars", help="Use G star models", action="store_true")
+    parser.add_argument("--wdstars", help="Use WD star models", action="store_true")
+    parser.add_argument(
+        "--primeonly", action="store_true", help="Only use prime designated sources"
+    )
+    parser.add_argument(
+        "--secondonly",
+        action="store_true",
+        help="Only use secondary designated sources",
+    )
+    parser.add_argument(
+        "--part1", action="store_true", help="Only check the part1 limited set of modes"
+    )
+    parser.add_argument(
+        "-t",
+        "--target_obs",
+        metavar=int,
+        default=3,
+        help="number of target observations per mode",
+    )
     return parser
 
 
-def which_observable(modewave, modemin, modemax,
-                     starnames, starwaves, starfluxes):
+def which_observable(modewave, modemin, modemax, starnames, starwaves, starfluxes):
     """
     Determine which stars can be observed in this mode
 
@@ -105,8 +146,7 @@ def which_observable(modewave, modemin, modemax,
     return obsstars
 
 
-def which_modes(mmvals,
-                starnames, starwaves, starfluxes):
+def which_modes(mmvals, starnames, starwaves, starfluxes):
     """
     Determine which modes are observable by which star
 
@@ -129,15 +169,20 @@ def which_modes(mmvals,
     starmodes = {}
     starmodes_num = {}
     for k in range(len(mmvals)):
-        if mmvals['sub_max'][k] > 0:
-            bandmax = mmvals['sub_max'][k]
+        if mmvals["sub_max"][k] > 0:
+            bandmax = mmvals["sub_max"][k]
         else:
-            bandmax = mmvals['full_max'][k]
-        obsnames = which_observable(mmvals['wave'][k], mmvals['full_min'][k],
-                                    bandmax,
-                                    starnames, starwaves, starfluxes)
+            bandmax = mmvals["full_max"][k]
+        obsnames = which_observable(
+            mmvals["wave"][k],
+            mmvals["full_min"][k],
+            bandmax,
+            starnames,
+            starwaves,
+            starfluxes,
+        )
         for cname in obsnames:
-            modeid = (mmvals['inst'][k], mmvals['mmode'][k], mmvals['band'][k])
+            modeid = (mmvals["inst"][k], mmvals["mmode"][k], mmvals["band"][k])
             if cname not in starmodes.keys():
                 starmodes[cname] = []
                 starmodes_num[cname] = 0
@@ -147,7 +192,7 @@ def which_modes(mmvals,
     return (starmodes, starmodes_num)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = initialize_parser()
     args = parser.parse_args()
@@ -181,30 +226,29 @@ if __name__ == '__main__':
     ysize = 9.0
     fig, cax = plt.subplots(figsize=(xsize, ysize))
 
-    set_params(lw=1., fontsize=16)
+    set_params(lw=1.0, fontsize=16)
 
     starfluxes = {}
     starwaves = {}
     for cname in allstarnames:
         cfile = glob.glob("data/%s_mod_0??.fits" % cname)
-        rb_filename = cfile[0].replace('.fits', '_r3000.fits')
+        rb_filename = cfile[0].replace(".fits", "_r3000.fits")
         ctable = Table.read(rb_filename)
 
         if cname in astarnames:
-            col = 'g'
+            col = "g"
         elif cname in gstarnames:
-            col = 'r'
+            col = "r"
         elif cname in wdstarnames:
-            col = 'b'
+            col = "b"
 
-        x = ctable['WAVELENGTH'].quantity
-        indxs, = np.where((x > 0.6*u.um) & (x < 29.*u.um))
-        x = ctable['WAVELENGTH'][indxs].quantity
-        flux = ctable['FLUX'][indxs].quantity*FLAM
+        x = ctable["WAVELENGTH"].quantity
+        (indxs,) = np.where((x > 0.6 * u.um) & (x < 29.0 * u.um))
+        x = ctable["WAVELENGTH"][indxs].quantity
+        flux = ctable["FLUX"][indxs].quantity * FLAM
         flux_mJy = flux.to(u.mJy, u.spectral_density(x))
 
-        cax.plot(x, (x**2)*flux_mJy,
-                 col+'-', label=cname)
+        cax.plot(x, (x ** 2) * flux_mJy, col + "-", label=cname)
 
         # save star fluxes and wavelengths
         starfluxes[cname] = flux_mJy
@@ -212,16 +256,20 @@ if __name__ == '__main__':
 
     # read in the min/max sensitivities for the modes
     if args.part1:
-        sens_filename = 'jwst_inst_sens_part1.dat'
+        sens_filename = "jwst_inst_sens_part1.dat"
     else:
-        sens_filename = 'jwst_inst_sens.dat'
-    mmvals = Table.read(sens_filename,
-                        format='ascii.commented_header',
-                        header_start=-1)
+        sens_filename = "jwst_inst_sens.dat"
+    mmvals = QTable.read(sens_filename, format="ascii.commented_header", header_start=-1)
+
+    # add in the units
+    mmvals['wave'] *= u.micron
+    mmvals['full_min'] *= u.mJy
+    mmvals['full_max'] *= u.mJy
+    mmvals['sub_max'] *= u.mJy
 
     # create a dictionary with all keys for all the modes
     #   using a tuple of (inst, mmode, band) as the key
-    mo_keys = zip(mmvals['inst'], mmvals['mmode'], mmvals['band'])
+    mo_keys = zip(mmvals["inst"], mmvals["mmode"], mmvals["band"])
     modeobserved = {}
     modeobservedstars = {}
     modedone = {}
@@ -237,21 +285,22 @@ if __name__ == '__main__':
     cstarfluxes = starfluxes.copy()
     obsstarlist = []
     while True:
-        starmodes, sm_num = which_modes(mmvals[indxs],
-                                        cstarnames, cstarwaves, cstarfluxes)
+        starmodes, sm_num = which_modes(
+            mmvals[indxs], cstarnames, cstarwaves, cstarfluxes
+        )
         # get the starname with the most observed modes
-        sname = ''
+        sname = ""
         maxobs = 0
         for cname in sm_num.keys():
             if sm_num[cname] > maxobs:
                 sname = cname
                 maxobs = sm_num[cname]
-            if cname == 'p330e':  # prioritize p330e
+            if cname == "p330e":  # prioritize p330e
                 sname = cname
                 maxobs = 1000
 
         # stop if no star covers the remaining modes
-        if sname == '':
+        if sname == "":
             break
 
         # add this star to the list for observations
@@ -272,58 +321,68 @@ if __name__ == '__main__':
 
         # check the list of modes, remove a mode if it has the target number
         for cur_mokey in modeobserved:
-            if (modeobserved[cur_mokey] >= target_num_obs):
+            if modeobserved[cur_mokey] >= target_num_obs:
                 if not modedone[cur_mokey]:
                     modedone[cur_mokey] = True
                     cinst, cmmode, cband = cur_mokey
-                    dindxs, = np.where((mmvals['inst'] == cinst)
-                                       & (mmvals['mmode'] == cmmode)
-                                       & (mmvals['band'] == cband))
-                    dindxs2, = np.where(dindxs[0] == indxs)
+                    (dindxs,) = np.where(
+                        (mmvals["inst"] == cinst)
+                        & (mmvals["mmode"] == cmmode)
+                        & (mmvals["band"] == cband)
+                    )
+                    (dindxs2,) = np.where(dindxs[0] == indxs)
                     del indxs[dindxs2[0]]
 
         # stop if no stars left
         if len(cstarnames) <= 0:
             break
 
-    print('star list')
+    print("star list")
     print(obsstarlist)
 
-    mo_keys = zip(mmvals['inst'], mmvals['mmode'], mmvals['band'])
-    print('%8s  %8s  %8s  %2s  %s' % ('Inst', 'MMode', 'Band', '#', 'stars'))
+    mo_keys = zip(mmvals["inst"], mmvals["mmode"], mmvals["band"])
+    print("%8s  %8s  %8s  %2s  %s" % ("Inst", "MMode", "Band", "#", "stars"))
     for ckey in mo_keys:
-        print('%8s,  %8s,  %8s,  %2i' % (ckey[0], ckey[1], ckey[2],
-                                         modeobserved[ckey]),
-              modeobservedstars[ckey])
+        print(
+            "%8s,  %8s,  %8s,  %2i" % (ckey[0], ckey[1], ckey[2], modeobserved[ckey]),
+            modeobservedstars[ckey],
+        )
 
     # plot the min/max sensitivites
-    uinst = np.unique(mmvals['inst'])
-    ctype = {'NIRCAM': 'c', 'NIRSPEC': 'm',
-             'NIRISS': 'y', 'MIRI': 'k', 'FGS': 'b'}
-    ptype = {'NIRCAM': 'solid', 'NIRSPEC': 'dashed',
-             'NIRISS': 'dotted', 'MIRI': 'dashdot', 'FGS': 'dashdot'}
+    uinst = np.unique(mmvals["inst"])
+    ctype = {"NIRCAM": "c", "NIRSPEC": "m", "NIRISS": "y", "MIRI": "k", "FGS": "b"}
+    ptype = {
+        "NIRCAM": "solid",
+        "NIRSPEC": "dashed",
+        "NIRISS": "dotted",
+        "MIRI": "dashdot",
+        "FGS": "dashdot",
+    }
     for cinst in uinst:
-        iindxs, = np.where(mmvals['inst'] == cinst)
-        umode = np.unique(mmvals['mmode'][iindxs])
+        (iindxs,) = np.where(mmvals["inst"] == cinst)
+        umode = np.unique(mmvals["mmode"][iindxs])
         for cmode in umode:
-            mindxs, = np.where(mmvals['mmode'][iindxs] == cmode)
+            (mindxs,) = np.where(mmvals["mmode"][iindxs] == cmode)
             for k in mindxs:
                 ll = iindxs[k]
-                bandname = mmvals['band'][ll]
-                bandmin = mmvals['full_min'][ll]
-                if mmvals['sub_max'][ll] > 0:
-                    bandmax = mmvals['sub_max'][ll]
+                bandname = mmvals["band"][ll]
+                bandmin = mmvals["full_min"][ll]
+                if mmvals["sub_max"][ll] > 0:
+                    bandmax = mmvals["sub_max"][ll]
                 else:
-                    bandmax = mmvals['full_max'][ll]
-                cwave = mmvals['wave'][ll]
-                cax.plot([cwave, cwave],
-                         np.array([bandmax, bandmin])*cwave**2,
-                         color=ctype[cinst], linestyle=ptype[cinst],
-                         linewidth=2.)
+                    bandmax = mmvals["full_max"][ll]
+                cwave = mmvals["wave"][ll]
+                cax.plot(
+                    [cwave.value, cwave.value],
+                    np.array([bandmax.value, bandmin.value]) * cwave.value ** 2,
+                    color=ctype[cinst],
+                    linestyle=ptype[cinst],
+                    linewidth=2.0,
+                )
 
-    cax.set_xscale('log')
+    cax.set_xscale("log")
     cax.set_xlim(0.6, 29.0)
-    cax.set_yscale('log')
+    cax.set_yscale("log")
     # cax.set_ylim(a_yrange)
     cax.set_ylabel(r"$\lambda^2 F(nu)$ [mJy $\mu m^2$]")
     # cax.legend()
@@ -331,16 +390,16 @@ if __name__ == '__main__':
     fig.tight_layout()
 
     # save the plot
-    basename = 'jwst_abscal_spec'
+    basename = "jwst_abscal_spec"
     if args.astars:
-        basename += '_astars'
+        basename += "_astars"
     if args.gstars:
-        basename += '_gstars'
+        basename += "_gstars"
     if args.wdstars:
-        basename += '_wdstars'
+        basename += "_wdstars"
     if args.part1:
-        basename += '_part1'
+        basename += "_part1"
     if args.savefig:
-        fig.savefig('%s.%s' % (basename, args.savefig))
+        fig.savefig("%s.%s" % (basename, args.savefig))
     else:
         plt.show()
